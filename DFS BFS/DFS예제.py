67576a0e62355d6bb3b862ce36cff85855e13ba9
rkdals0203 <rkdals0203@gmail.com>
2024-03-01 14:37:39 +0000

    DFS

--- DFS BFS/DFS예제.py
+++ DFS BFS/DFS예제.py
@@ -1,35 +1,3 @@
-n,m = map(int,input().split())
-
-graph= []
-
-for _ in range (n):
-  row = list(map(int,input()))
-  graph.append(row)
-print(graph)
-
-def dfs(y,x):
-  if x<0 or y<0 or x>=m or y>=m:
-    return False
-  if graph[y][x]==0:
-    graph[y][x]=1
-    dfs(y-1,x)
-    dfs(y+1,x)
-    dfs(y,x-1)
-    dfs(y,x+1)
-    return True
-  else:
-    return False
-
-ans = 0
-
-for i in range (n):
-  for j in range (m):
-    if dfs(i,j) == True:
-      ans+=1
-print(ans)
-
-
-#2
 def dfs(y,x):
   #정해진 틀 이외의 접근은 바로 리턴해버림
   if y<0 or y>=n or x<0 or x>=m:
@@ -45,12 +13,13 @@ def dfs(y,x):
   #현재 좌표가 1이라면
   else:
     return False
-
+  
 #그래프 만들기
 n,m = map(int,input().split())
 graph= []
 for _ in range (n):
   graph.append(list(map(int,input())))
+print(graph)
 
 count = 0 
 for i in range(n):
@@ -59,6 +28,10 @@ for i in range(n):
       count+=1
 
 print(count)
+
+
+
+
 
 #visited 필요 없음. 왜냐하면 그래프에서 1로 표시해버리면 그만이거든
 #또한, 그래프는 인접리스트가 아니라 좌표임.
--- main.py
+++ main.py
@@ -1,49 +1,37 @@
-import sys
-input = sys.stdin.readline
+def dfs(y,x):
+  #정해진 틀 이외의 접근은 바로 리턴해버림
+  if y<0 or y>=n or x<0 or x>=m:
+    return False
+  #현재 좌표가 0이라면
+  if graph[y][x] == 0:
+    graph[y][x] = 1
+    dfs(y-1,x)
+    dfs(y+1,x)
+    dfs(y,x-1)
+    dfs(y,x+1)
+    return True
+  #현재 좌표가 1이라면
+  else:
+    return False
 
-def turn_left():
-  global direction
-  direction = direction-1
-  if direction == -1:
-    direction = 3
-  
+#그래프 만들기
+n,m = map(int,input().split())
+graph= []
+for _ in range (n):
+  graph.append(list(map(int,input())))
 
-n, m = map(int,input().split())
-a,b,direction = map(int,input().split())
-da = [-1,0,+1,0]
-db = [0,1,0,-1]
-
-game_map = []
-for _ in range(n):
-  arr = (list(map(int,input().split())))
-  game_map.append(arr)
-# 맵 구성. 1이 바다, 0이 육지
-
-game_map[a][b] = 1
-count = 1
-turn_time = 0
-movable = True
-while movable:
-  turn_left() #왼쪽으로 돌아서기
-  na = a + da[direction]
-  nb = b + db[direction]
-  if game_map[na][nb] == 0: #갈 수 있는 경우
-    game_map[na][nb] = 1
-    a = na
-    b = nb
-    count += 1
-    turn_time = 0
-  else: #갈 수 없는 경우, 회전 횟수 1 증가됨
-    turn_time += 1
-    if turn_time == 4:
-      na = a-da[direction]
-      nb = b-db[direction]
-      if game_map[na][nb] == 0:
-        a = na
-        b = nb
-        turn_time = 0
-      else:
-        movable = False
+count = 0 
+for i in range(n):
+  for j in range(m):
+    if dfs(i,j)==True:
+      count+=1
 
 print(count)
 
+
+
+
+
+#visited 필요 없음. 왜냐하면 그래프에서 1로 표시해버리면 그만이거든
+#또한, 그래프는 인접리스트가 아니라 좌표임.
+visited = [0]*9
