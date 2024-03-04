b7b8989965fd21f1ad858d6b79b57aa6d144937e
rkdals0203 <rkdals0203@gmail.com>
2024-03-04 13:40:46 +0000

    정렬1

--- main.py
+++ main.py
@@ -1,26 +1,10 @@
-# deque는 자유자재로 스택처럼, 혹은 큐처럼 사용할 수 있는 자료구조.
-from collections import deque
+array = [7,5,9,0,3,1,6,2,4,8]
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+for i in range(1,len(array)):
+  for j in range(i,0,-1):
+    if array[j]<array[j-1]:
+      array[j],array[j-1] = array[j-1],array[j]
+    else:
+      break
 
-def bfs(y,x):
-  queue = deque([(y,x)])
-  while queue: #queue가 비어있지 않을 때 까지
-    y,x = queue.popleft()
-    for i in range(4):
-      ny = y+dy[i]
-      nx = x+dx[i]
-      if ny>=0 and ny<n and nx>=0 and nx<m:
-        if graph[ny][nx] == 1: #괴물 없는 부분이라면
-          graph[ny][nx] = graph [y][x]+1
-          queue.append((ny,nx))
-
-# 미로 세팅
-n,m = map(int,input().split())
-graph = []
-for _ in range(n):
-  graph.append(list(map(int,input())))
-
-bfs(0,0)
-print(graph[n-1][m-1])
+print(array)
