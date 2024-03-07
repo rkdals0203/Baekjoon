11e01cb81dd351dbc6afe2fbe6abc198b39f9d69
rkdals0203 <rkdals0203@gmail.com>
2024-03-07 08:44:09 +0000

    이진탐색

--- Binary Search/이진탐색.py
+++ Binary Search/이진탐색.py
@@ -14,3 +14,22 @@ def binary_search(array, start, end, target):
 
 
 print(binary_search(array,0,9,21))
+
+
+#2
+array = [0,2,4,6,8,10,12,14,16,18]
+
+def binary_search2(start,end,target,array):
+  if start > end: #종료조건
+    return None
+
+  middle = (start+end)//2 #start:0이고 end:9인 경우 middle은 버림해서 4
+  if target==array[middle]:
+    return middle
+  elif target<array[middle]:
+    return binary_search(start,middle-1,target,array)
+  else:
+    return binary_search(middle+1,end,target,array)
+
+
+print(binary_search(0,9,6,array))
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-n,k = map(int,input().split())
-a = list(map(int,input().split()))
-b = list(map(int,input().split()))
-a.sort()
-b.sort(reverse=True)
+array = [0,2,4,6,8,10,12,14,16,18]
 
-for i in range(k):
-  if a[i]<b[i]:
-    a[i] = b[i]
-print(sum(a))
+def binary_search(start,end,target,array):
+  if start > end: #종료조건
+    return None
+    
+  middle = (start+end)//2 #start:0이고 end:9인 경우 middle은 버림해서 4
+  if target==array[middle]:
+    return middle
+  elif target<array[middle]:
+    return binary_search(start,middle-1,target,array)
+  else:
+    return binary_search(middle+1,end,target,array)
 
+
+print(binary_search(0,9,6,array))
