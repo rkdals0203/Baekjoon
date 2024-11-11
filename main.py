69dc90cd4e0d2b6e01cacd55f326477109b4ea47
rkdals0203 <rkdals0203@gmail.com>
2024-11-11 21:45:21 +0900

    곱하기혹은더하기 2트

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-n,k = map(int, input().split())
-count = 0
+s = input()
+arr = []
+for j in range(len(s)):
+  arr.append(int(s[j]))
+res = int(s[0])
 
-if n<k:
-  count = n-1
+for i in range (1, len(s)):
+  add = res + arr[i]
+  mod = res * arr[i]
+  if add >= mod:
+    res = add
+  else:
+    res = mod
 
-else:
-  while n>=k:
-    #빼는 부분(나눠지지 않을 때만 진짜로 뺌)
-    target = (n//k)*k
-    count += n-target
-    n = target
-    #나누는 부분
-    n = n//k
-    count+=1
-print(count)
+print(res)
