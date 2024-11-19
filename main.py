f6e28cd9c480b32df3fd95305bd372161715d5c1
rkdals0203 <rkdals0203@gmail.com>
2024-11-19 17:10:29 +0900

    볼링공 고르기

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-n = int(input())
-coins = list(map(int, input().split()))
-coins.sort()
+n, m = map(int, input().split())
+k = list(map(int, input().split()))
+k.sort()
 res = 0
-sum = 0
 
-for i in range(len(coins)):
-    #작은 애들부터 더한거+1 하면 다음거보다 크거나 같아?
-    if sum +1 >= coins[i]:
-        #그러면 합류시켜
-        sum += coins[i]
-#반복문 끝나고나서 sum에 1더한게 답
-res = sum+1
+for i in range(len(k)):
+    temp = k[i]
+    for j in range(i,len(k)):
+        if temp != k[j]:
+            res+=1
 print(res)
