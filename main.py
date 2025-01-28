590d9eac66083c287221fc5a260fe95b244b569d
rkdals0203 <rkdals0203@gmail.com>
2025-01-28 22:47:38 +0900

    문자열 압축

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 s = input()
-s = "ababababcc"
 compressed = []
 
-for slice in range(1,len(s)+1): #1~8까지 슬라이스 개수를 키우기 위한 반복문
-    sliced = [s[j:j+slice] for j in range(0, len(s), slice)]
-    for k in range(len(sliced)):
-        index = 0
-        if sliced[k-1] == sliced[k]:
+for slice_index in range(1, len(s) + 1):
+    sliced = [s[j:j + slice_index] for j in range(0, len(s), slice_index)]  # 슬라이스 된 문자열
+    result = []
+    index = 1
+
+    for k in range(1, len(sliced)):
+        if sliced[k - 1] == sliced[k]:
             index += 1
         else:
-            sliced[k] = str(index) + sliced[k]
-    compressed.append(sliced)
+            if index > 1:
+                result.append(str(index) + sliced[k - 1])
+            else:
+                result.append(sliced[k - 1])
+            index = 1
+
+    # 마지막 남은 패턴 추가
+    if index > 1:
+        result.append(str(index) + sliced[-1])
+    else:
+        result.append(sliced[-1])
+
+    compressed.append("".join(result))
 
 print(compressed)
